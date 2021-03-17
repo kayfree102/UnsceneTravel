f71b93b42538373c1139fddcc627dae42812f88c
Kendal <kendalmfreeman@gmail.com>
2021-03-17 18:08:46 -0500

    Initial Commit

---
+++ UnsceneTravel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnsceneTravel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
